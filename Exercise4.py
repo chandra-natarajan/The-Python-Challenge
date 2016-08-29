0af99cccf3bd5f8a653d6bf5e531d26d2fd4b4d8
cmnatarajan <moulic@gmail.com>
2016-08-29 22:21:49 +0530

    Exercise4.py

--- Exercise4.py
+++ Exercise4.py
@@ -7,21 +7,24 @@ Created on Sat Aug 27 11:34:05 2016
 from urllib.request import urlopen
 import re 
 
-htmlStart = r'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
-htmlEnd = r'12345'
-htmlEndArray=['12345']
-p = re.compile(r'next nothing is (\w+)')
-q = re.compile(r'\D')
-for i in range(250):
-	with urlopen(htmlStart+htmlEnd) as response:
-		for line in response:
-			line = line.decode('utf-8')  # Decoding the binary data to text.
-			x = p.findall(line)
+def crawlNext( htmlEnd ):
+	htmlStart = r'http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='
+	p = re.compile(r'next nothing is (\w+)')
+	for i in range(250): #assume max iteration as 250
+		#print (i)
+		with urlopen(htmlStart+htmlEnd) as response:
+			for line in response:
+				line = line.decode('utf-8')  # Decoding the binary data to text.
+				x = p.findall(line)
 			if x:				
-				htmlEnd =x[0]
-				htmlEndArray.extend(x)
-				y = q.findall(x[0])
-				if y:
-					break				
+				htmlEnd = x[0]
+			else:
+				print (htmlEnd)
+				print(line)
+				break
+	return htmlEnd
 
-print (htmlEndArray)
+htmlEnd = r'12345'
+htmlEnd = crawlNext(htmlEnd)
+htmlEnd = str(int(htmlEnd)/2)
+htmlEnd = crawlNext(htmlEnd)
