2303d65c369450c8932c9cebda01427682b93d6e
cmnatarajan <moulic@gmail.com>
2016-08-30 08:00:49 +0530

    Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -5,9 +5,9 @@ Created on Sat Aug 29 10:33:05 2016
 @author: Mouli
 """
 from urllib.request import urlopen
-import pickle, pprint
-
+import pickle
 htmlFile = r'http://www.pythonchallenge.com/pc/def/banner.p'
-p = pickle.load(urlopen(htmlFile))
-for item in p:
-	print (item)
+for line in pickle.load(urlopen(htmlFile)):
+	for item in line:
+		print(item[0]*item[1], end='')
+	print()
